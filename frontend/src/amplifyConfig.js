a707f18cfba3f78321b28a20d193270a2b9c6b3f
Timothy Muhlsetein <122745865+timmuh03@users.noreply.github.com>
2026-05-21 17:19:38 -0400

    Update amplifyConfig.js

--- frontend/src/amplifyConfig.js
+++ frontend/src/amplifyConfig.js
@@ -1,7 +1,7 @@
 import { Amplify } from 'aws-amplify';
 
 const userPoolId = import.meta.env.VITE_COGNITO_USER_POOL_ID;
-const userPoolClientId = import.meta.env.VITE_CGNITO_USER_POOL_CLIENT_ID;
+const userPoolClientId = import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID;
 
 if (!userPoolId || !userPoolClientId) {
   throw new Error(
