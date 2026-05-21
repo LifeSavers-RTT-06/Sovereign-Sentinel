523b77760605fe1129ee4858c7daf900470a7e47
Timothy Muhlsetein <122745865+timmuh03@users.noreply.github.com>
2026-05-21 17:08:57 -0400

    Update amplifyConfig.js

--- frontend/src/amplifyConfig.js
+++ frontend/src/amplifyConfig.js
@@ -1,13 +1,21 @@
 import { Amplify } from 'aws-amplify';
 
+const uesrPoolId = import.meta.env.VITE_COGNITO_USER_POOL_ID;
+const userPoolClientId = import.meta.env.VITE_CGNITO_USER_POOL_CLIENT_ID;
+
+if (!userPoolId || !userPoolClientId) {
+  throw new Error(
+    "Missing Cognito configuration. Check VITE_COGNITO_USER_POOL_ID and VITE_COGNITO_USER_POOL_ID."
+  )
+}
+
 const amplifyConfig = {
   Auth: {
-    region: import.meta.env.VITE_AWS_REGION,
-    userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-    userPoolWebClientId: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID,
-    loginWith: {
-      email: true,
-      username: true,
+    Cognito: {
+      userPoolId,
+      userPoolClientId,
+      loginWith: {
+        email: true,
     },
   },
 };
