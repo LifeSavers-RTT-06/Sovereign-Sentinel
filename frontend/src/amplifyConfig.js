a95bd8e2ae3a1b96eeddd1a870f13203a99a727c
Timothy Muhlsetein <122745865+timmuh03@users.noreply.github.com>
2026-05-21 17:18:02 -0400

    Update amplifyConfig.js

--- frontend/src/amplifyConfig.js
+++ frontend/src/amplifyConfig.js
@@ -5,7 +5,7 @@ const userPoolClientId = import.meta.env.VITE_CGNITO_USER_POOL_CLIENT_ID;
 
 if (!userPoolId || !userPoolClientId) {
   throw new Error(
-    "Missing Cognito configuration. Check VITE_COGNITO_USER_POOL_ID and VITE_COGNITO_USER_POOL_ID."
+    "Missing Cognito configuration. Check VITE_COGNITO_USER_POOL_ID and VITE_COGNITO_USER_POOL_CLIENT_ID."
   )
 }
 
